dd503f5a5d2fe63f3f7c9c63d6c5662a7ea1425b
Marc <>
2022-04-24 18:31:00 +0800

    Heroku

--- dist/app.js
+++ dist/app.js
@@ -5,13 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 app.get('/', (req, res) => {
     res.send('The server is working!');
 });
-app.listen(port, () => {
-    if (port === 3000) {
-        console.log('true');
-    }
-    console.log(`server is listening on ${port} !!!`);
+app.listen(process.env.PORT, () => {
+    console.log(`server is listening on ${process.env.PORT} env!!!`);
 });
