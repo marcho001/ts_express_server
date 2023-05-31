a7649bf38f84840b0192c9b2db0aba5c28b4670d
marcho001 <g40419@gmail.com>
2023-05-31 18:45:40 +0800

    delete dist and add gitignore

--- dist/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const EdmApi_1 = require("./api/EdmApi");
-exports.default = (app) => {
-    app.use('/', EdmApi_1.router);
-};
