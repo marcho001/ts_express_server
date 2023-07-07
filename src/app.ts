c685194f8adb4a6e76ce2a4f980d45117cf51245
marcho001 <g40419@gmail.com>
2023-07-07 18:08:25 +0800

    feat 加入 express-session 套件

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import * as dotenv from 'dotenv'
+import session from 'express-session'
 
 const app = express()
 
@@ -9,6 +10,19 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config()
 }
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      domain: process.env.SESSION_DOMAIN,
+      maxAge: 1000 * 60 * 60 * 12
+    },
+
+    rolling: true
+  })
+)
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
